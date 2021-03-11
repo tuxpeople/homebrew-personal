0b29d221068e8ccb198134dccd2fff39178cfc24
Thomas Deutsch <thomas@tuxpeople.org>
2021-03-11 14:23:53 +0100

    Update tuxpeople-font-bs.rb

--- Casks/tuxpeople-font-bs.rb
+++ Casks/tuxpeople-font-bs.rb
@@ -2,7 +2,7 @@ cask "tuxpeople-font-bs" do
   version "1.00"
   sha256 "c4b90ff7aa964e647dcb1f9a1d94230c4c04d256ad5e5e8151f56b393d38c175"
 
-  url "https://cloud.eighty-three.me/s/xkFx6NkHZrGixNW/download"
+  url "https://www.dropbox.com/s/ap10m5bnxfvbgto/BAHNSCHRIFT.zip?dl=1"
   name "Bahnschrift"
   homepage "https://de.wikipedia.org/wiki/Bahnschrift"
 
