e49d1a4dcc4cff8031d28a1805c9ce1f9f1def2c
Thomas Deutsch <thomas@tuxpeople.org>
2022-12-14 14:17:19 +0100

    Create tuxpeople-font-basisschrift.rb

---
+++ Casks/tuxpeople-font-basisschrift.rb
@@ -0,0 +1,11 @@
+cask "tuxpeople-font-dch-basisschrift" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.basisschrift.ch/sites/default/files/DCH-Basisschrift.otf.zip"
+  name "Deutschschweizer Basisschrift"
+  desc "Die Deutschschweizer Basisschrift ist eine didaktische Schrift, die für den Handschrifterwerb konzipiert worden ist. Sie ist bewusst keine Satzschrift, die gestalterische Möglichkeiten bei der Erstellung von Arbeitsblättern und Materialien zulässt. Es gibt daher auch keine fette oder kursive Version der Schrift. Das Konzept der Deutschschweizer Basisschrift setzt bewusst auf handschrifltiche Vorlagen von Lehrpersonen."
+  homepage "https://moji-waku.com/albertella/index.html"
+
+  font "DCH-Basisschrift.otf"
+end
